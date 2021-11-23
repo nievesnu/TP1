a891cb848d62c43bf65b63592423f872c2cbe65b
Nieves Núñez <nievesnu@ucm.es>
2021-11-23 20:00:16 +0100

    super cars commands almost ready 23 de nov

---
+++ SuperCars_23/src/es/ucm/tp1/supercars/logic/gameobjects/Truck.java
@@ -0,0 +1,61 @@
+package es.ucm.tp1.supercars.logic.gameobjects;
+
+import es.ucm.tp1.supercars.logic.Collider;
+import es.ucm.tp1.supercars.logic.Game;
+
+public class Truck extends GameObject{
+
+	public boolean crashed;
+	public static final String SYMBOL = "←";
+	public static final Object INFO = null;
+	
+	public Truck(Game game, int x, int y) {
+		super(game, x, y);
+		this.crashed = false;
+		this.symbol = SYMBOL;
+	}
+	
+	public boolean isCrashed() {
+		return crashed;
+	}
+	
+	public void crash() {
+		this.crashed = true;
+	}
+	
+	public boolean doCollision() {
+		return false;
+	}
+
+	@Override
+	public boolean receiveCollision(Player player) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean receiveShoot(Player player) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void onEnter() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onDelete() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+}
