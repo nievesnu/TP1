7166f847f395d115fba2dbf7d0408e58d647b892
Nieves Núñez <nievesnu@ucm.es>
2021-11-28 18:56:55 +0100

    delete things

--- SuperCars.2.1/src/es/ucm/tp1/supercars/logic/actions/InstantAction.java
+++
@@ -1,7 +0,0 @@
-package es.ucm.tp1.supercars.logic.actions;
-
-import es.ucm.tp1.supercars.logic.Game;
-
-public interface InstantAction {
-	void execute(Game game);
-}
--- SuperCars_25/src/es/ucm/tp1/supercars/logic/InstantAction.java
+++
@@ -1,5 +0,0 @@
-package es.ucm.tp1.supercars.logic;
-
-public interface InstantAction {
-	void execute(Game game);
-}
